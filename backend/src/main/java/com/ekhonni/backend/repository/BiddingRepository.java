0b3d0da590db23f36d79e49d2c2a7ba24fae917f
Alamin-Bhuiyan <amialaminb@gmail.com>
2023-09-27 15:58:45 +0600

    adding swagger

--- backend/src/main/java/com/ekhonni/backend/repository/BiddingRepository.java
+++ backend/src/main/java/com/ekhonni/backend/repository/BiddingRepository.java
@@ -2,6 +2,8 @@ package com.ekhonni.backend.repository;
 
 import com.ekhonni.backend.model.Bidding;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BiddingRepository extends CrudRepository<Bidding, Long> {
 }
