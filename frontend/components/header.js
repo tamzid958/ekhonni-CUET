899d1eefd53b96a16b37d7f1f27005d29da3e07b
AshifurNahid <ashifurnahid@gmail.com>
2023-09-27 14:13:36 +0600

    Reuseable components are done

---
+++ frontend/components/header.js
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Header() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+        <Navbar.Brand href="#home" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            Ekhoni
+            <div style={{ fontSize: '14px', color: 'lightgray' }}>
+              Buy and Sell Anything
+            </div>
+          </Navbar.Brand>
+
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/profile">Profile</Nav.Link>
+            <Nav.Link href="#pricing">Post your AD</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+     
+    </>
+  );
+}
+
+export default Header;
