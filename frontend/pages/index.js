a643a741db19006e17ebe9f2e19b47883bbc0855
AshifurNahid <ashifurnahid@gmail.com>
2023-10-02 10:45:31 +0600

    some bug fixed

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,7 @@
-import Head from 'next/head'
-import '../styles/Home.module.css'
-import ProductCard from '../components/card/productCard'
-import ColorfulCarousel from '../components/carousel/carousel'
-import Image from 'next/image'
 import Footer from '../components/footer'
 import Header from '../components/header'
-import SearchBar from '../components/searchbar'
-
-import CategoryList from '../components/categorylist'
 import CarouselSection from '../components/carousel/carouselSection'
-import CardSection from '../components/Card/cardSection'
+import CardSection from '../components/card/cardSection'
 
 export default function Home() {
   
