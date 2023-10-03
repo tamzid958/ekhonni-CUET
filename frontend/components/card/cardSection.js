eb4bd27f225dd1f25f9ff6a83a519b984f71deef
rifaturrana <smrifaturrana@gmail.com>
2023-10-03 13:42:18 +0600

    bug fixed

--- frontend/components/card/cardSection.js
+++ frontend/components/card/cardSection.js
@@ -1,16 +1,5 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import ProductCard from './productCard';
-
-export const getStaticProps = async () => {
-  const result = await fetch("http://localhost:8080/products")
-  const data = await result.json()
-  return {
-    props: {}
-  }
-}
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function CardSection({ data }) {
   console.log(data);
 
@@ -18,16 +7,13 @@ function CardSection({ data }) {
     <div className='p-5'>
       <div className='border border-1 border-solid border-ddd p-5 mb-5'>
         <div className='row'>
-          {products.map((product) => (
-            <div key={product.id} className='col-sm-3'>
-              <ProductCard product={product} />
-            </div>
-          ))}
+          <div className='col-sm-3'>
+            <ProductCard product={data} />
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default CardSection;
-
+export default CardSection
